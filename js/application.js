c6e34f5779af8f83c83c39a09274b7b598700942
teelu <terrence.lun@gmail.com>
2015-07-13 14:28:10 +0800

    Added Cup Objects

--- js/application.js
+++ js/application.js
@@ -1,3 +1,38 @@
 $(document).ready(function() {
-	console.log("READY");
+  //cup holder(har har har)
+  var cupArray = [];
+
+
+
+  //cup functions
+  function cup(name,position,hasBall) {
+    name = name || "tempCup";
+    position = position || 0;
+    hasBall = hasBall || false;
+    this.name = name;
+    this.position = position;
+    this.hasBall = hasBall;
+    this.getKeyFromValue = function(value) {
+      for(var keys in this) {
+        if (this[keys]==value) {
+          return this;
+        }
+      }
+    }
+  }
+
+  //generate cups
+  for (var i = 0; i < 3; i++) {
+    cupArray.push(new cup("cup" + eval('i+1'),i,false));
+  }
+
+  console.log(cupArray);
+
+  //Pick a cup
+  console.log("Where to insert ball");
+  var cupSelected = prompt();
+  //set cup selected
+  cupArray[cupSelected].hasBall = true;
+
+
 });
