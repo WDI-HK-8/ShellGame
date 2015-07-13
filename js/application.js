46c09850c996d4c32b651cf9738e89e92ea7c257
teelu <terrence.lun@gmail.com>
2015-07-13 21:47:48 +0800

    Moved functions into game function.  added game.start on document ready and splash screen

--- js/application.js
+++ js/application.js
@@ -1,13 +1,11 @@
-$(document).ready(function() {
-  //cup holder(har har har)
-  var cupArray = [];
-  //Holder for moves objects
-  var movesArray = [];
+//Game Class
+function Game() {
+  //variables
+  this.cupArray = [];
+  this.movesArray = [];
 
-
-
-  //cup functions
-  function cup(name,hasBall) {
+  //cup constructor
+  this.cup = function (name,hasBall) {
     this.name = name || "tempCup";
     this.hasBall = hasBall || false;
     this.getKeyFromValue = function(value) {
@@ -18,21 +16,29 @@ $(document).ready(function() {
       }
     }
   }
-  
+
+  this.createCups = function () {
+    //generate cups
+    for (var i = 0; i < 3; i++) {
+      this.cupArray.push(new this.cup("cup" + eval('i+1'),false));
+    }
+  }
+
   //Move constructor
-  function move(position,direction) {
+  this.move = function (position,direction) {
     this.position = position;
     this.direction = direction;
   }
 
-  function executeMove(move, arr) {
+  //methods
+  this.executeMove = function (move) {
     var cupMovePosition = move.position;
     var cupMoveDirection = move.direction;
     var cupAffectedPosition;
     var tempArr = [];
     //deep copy cupArray
-    for (var i = 0; i < cupArray.length; i++) {
-      tempArr.push(cupArray[i]);
+    for (var i = 0; i < this.cupArray.length; i++) {
+      tempArr.push(this.cupArray[i]);
     }
     if (cupMoveDirection == 'left') {
       if (cupMovePosition > 0) {
@@ -48,38 +54,35 @@ $(document).ready(function() {
       }
     }
     
-    tempCup = cupArray[cupMovePosition];
+    tempCup = this.cupArray[cupMovePosition];
     tempArr[cupMovePosition] = tempArr[cupAffectedPosition];
     tempArr[cupAffectedPosition] = tempCup;
 
     return tempArr;
   }
 
+  
 
+  this.start = function () {
+    console.log("game Started!");
+    this.createCups();
+    this.move1 = new this.move(1,'right');
 
-  //generate cups
-  for (var i = 0; i < 3; i++) {
-    cupArray.push(new cup("cup" + eval('i+1'),false));
+    console.log(this.cupArray);
+    this.cupArray = this.executeMove(this.move1);
   }
 
-  console.log(cupArray);
+}
 
-  //Pick a cup
-  console.log("Where to insert ball");
-  //set cup selected
-  var cupSelected = prompt();
+$(document).ready(function() {
 
-  cupArray[cupSelected].hasBall = true;
-  //Test moves
-  var move1 = new move(0,'left');
-  var move2 = new move(1,'left');
-  var move3 = new move(2,'right');
-  var move4 = new move(1,'right');
-  cupArray = executeMove(move1,cupArray);
-  console.log(cupArray);
-  cupArray = executeMove(move2,cupArray);
-  console.log(cupArray);
 
+  var game = new Game();
+  $('#game-start').click(function() {
+    game.start();
+    console.log(game.cupArray);
+    $('.splash-screen').remove();
+  });
 
 
 });
