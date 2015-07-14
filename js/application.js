43852a124282730067b5ca274a2bd20465d687e7
teelu <terrence.lun@gmail.com>
2015-07-14 15:07:58 +0800

    Added line break and added a space after every comma

--- js/application.js
+++ js/application.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
   //cup holder(har har har)
   var cupArray = [];
   //cup functions
-  function cup(name,position,hasBall) {
+  function cup(name, position, hasBall) {
     this.name = name;
     this.position = position;
     this.hasBall = hasBall;
   }
   //generate cups
   for (var i = 0; i < 3; i++) {
-    cupArray.push(new cup("cup" + eval('i+1'),i,false));
+    cupArray.push(new cup("cup" + eval('i+1'), i, false));
   }
   console.log(cupArray);
   //Pick a cup
