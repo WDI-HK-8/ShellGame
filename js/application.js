affce09ce044d7b642f8ddfc6cfbe2c67ca4471f
teelu <terrence.lun@gmail.com>
2015-07-14 14:28:04 +0800

    removed white space

--- js/application.js
+++ js/application.js
@@ -1,39 +1,27 @@
 function rand(limit) {
   return Math.floor((Math.random()*(limit+1)));
 }
-
 //Game Class
 function Game() {
   //variables
   this.cupArray = [];
   this.movesArray = [];
-
   //cup constructor
   this.cup = function (name,hasBall) {
     this.name = name || "tempCup";
     this.hasBall = hasBall || false;
-    this.getKeyFromValue = function(value) {
-      for(var keys in this) {
-        if (this[keys]==value) {
-          return this;
-        }
-      }
-    }
   }
-
   this.createCups = function () {
     //generate cups
     for (var i = 0; i < 3; i++) {
       this.cupArray.push(new this.cup("cup" + eval('i+1'),false));
     }
   }
-
   //Move constructor
   this.move = function (position,direction) {
     this.position = position;
     this.direction = direction;
   }
-
   //methods
   this.executeMove = function (move) {
     var cupMovePosition = move.position;
@@ -61,10 +49,8 @@ function Game() {
     tempCup = this.cupArray[cupMovePosition];
     tempArr[cupMovePosition] = tempArr[cupAffectedPosition];
     tempArr[cupAffectedPosition] = tempCup;
-
     return tempArr;
   }
-
   this.generateComputer = function () {
     for (var i = 0; i < 10; i++) {
       //generate numbers for move
@@ -75,21 +61,15 @@ function Game() {
       this.movesArray.push(new this.move(randomPosition,randomDirectionString));
     }
   }
-
   this.start = function () {
     console.log("game Started!");
     this.createCups();
     this.move1 = new this.move(1,'right');
-
     console.log(this.cupArray);
     this.cupArray = this.executeMove(this.move1);
   }
-
 }
-
 $(document).ready(function() {
-
-
   var game = new Game();
   $('#game-start').click(function() {
     game.start();
@@ -98,6 +78,4 @@ $(document).ready(function() {
     $('.splash-screen').remove();
     console.log(game.movesArray);
   });
-
-
 });
