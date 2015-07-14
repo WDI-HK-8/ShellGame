73060a9a6b61fc3187a93cf4ffd0f38dd9a40326
teelu <terrence.lun@gmail.com>
2015-07-14 14:15:09 +0800

    removed spacing and fixed indentations

--- js/application.js
+++ js/application.js
@@ -1,9 +1,7 @@
+'use strict';
 $(document).ready(function() {
   //cup holder(har har har)
   var cupArray = [];
-
-
-
   //cup functions
   function cup(name,position,hasBall) {
     name = name || "tempCup";
@@ -12,27 +10,15 @@ $(document).ready(function() {
     this.name = name;
     this.position = position;
     this.hasBall = hasBall;
-    this.getKeyFromValue = function(value) {
-      for(var keys in this) {
-        if (this[keys]==value) {
-          return this;
-        }
-      }
-    }
   }
-
   //generate cups
   for (var i = 0; i < 3; i++) {
     cupArray.push(new cup("cup" + eval('i+1'),i,false));
   }
-
   console.log(cupArray);
-
   //Pick a cup
   console.log("Where to insert ball");
   var cupSelected = prompt();
   //set cup selected
   cupArray[cupSelected].hasBall = true;
-
-
 });
